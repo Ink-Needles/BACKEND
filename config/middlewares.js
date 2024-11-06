610480925262f07e4984937ffa3d99751ef9681a
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-11-07 01:02:00 +0200

    Real initial commit

---
+++ config/middlewares.js
@@ -0,0 +1,42 @@
+module.exports = [
+  'strapi::errors',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "storage.googleapis.com",
+            "dl.airtable.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "storage.googleapis.com",
+            "dl.airtable.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  'strapi::cors',
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  {
+    name: 'strapi::body',
+    config: {
+      includeUnparsed: true, // This allows the raw body to be available
+    },
+  },
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
