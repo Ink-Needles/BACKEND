a2cc54067fc5302ba15d2313e81ac50a0a999cd0
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-12-03 20:44:13 +0200

    😗 Update user registration to set confirmed status to true

--- src/api/auth/controllers/custom-register.js
+++ src/api/auth/controllers/custom-register.js
@@ -29,7 +29,7 @@ module.exports = {
           email,
           username,
           password,
-          confirmed: false,
+          confirmed: true,
           confirmationToken: strapi.plugins['users-permissions'].services.jwt.issue({ email }),
           role: authenticatedRole.id,
         },
