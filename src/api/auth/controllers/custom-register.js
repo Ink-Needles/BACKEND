4aadfe1634d6405e57db62e38ce126c513b3ee5a
VencislavManoilov <93076150+VencislavManoilov@users.noreply.github.com>
2024-12-04 19:06:26 +0200

    ✌ Refactor user registration logic to handle Google sign-in and adjust confirmation email flow

--- src/api/auth/controllers/custom-register.js
+++ src/api/auth/controllers/custom-register.js
@@ -23,47 +23,37 @@ module.exports = {
       where: { type: 'authenticated' },
     });
 
-    if(google) {
-      const newUser = await strapi.query('plugin::users-permissions.user').create({
-        data: {
-          email,
-          username,
-          password,
-          confirmed: true,
-          confirmationToken: null,
-          role: authenticatedRole.id,
-        },
-      });
-
-      const sanitizedUser = await sanitize.contentAPI.output(newUser, strapi.getModel('plugin::users-permissions.user'));
-
-      const jwt = strapi.plugins['users-permissions'].services.jwt.issue({ id: newUser.id });
-
-      return ctx.send({ jwt, user: sanitizedUser });
+    let hashedPassword = password;
+    if (google) {
+      hashedPassword = await strapi.plugins['users-permissions'].services.user.hashPassword({ password });
     }
 
     const newUser = await strapi.query('plugin::users-permissions.user').create({
       data: {
         email,
         username,
-        password,
-        confirmed: false,
-        confirmationToken: strapi.plugins['users-permissions'].services.jwt.issue({ email }),
+        password: hashedPassword,
+        confirmed: google ? true : false,
+        confirmationToken: google ? null : strapi.plugins['users-permissions'].services.jwt.issue({ email }),
         role: authenticatedRole.id,
       },
     });
 
     const sanitizedUser = await sanitize.contentAPI.output(newUser, strapi.getModel('plugin::users-permissions.user'));
 
-    // Send confirmation email
-    await strapi.plugins['email'].services.email.send({
-      to: email,
-      from: 'vencislav.developer@gmail.com',
-      subject: 'Email Confirmation',
-      text: `Please confirm your email by clicking on the following link: http://localhost:3000/confirmation?confirmation=${newUser.confirmationToken}`,
-      html: `<p>Please confirm your email by clicking on the following link:</p><a href="http://localhost:3000/confirmation?confirmation=${newUser.confirmationToken}">Confirm your email</a>`,
-    });
+    if (!google) {
+      // Send confirmation email
+      await strapi.plugins['email'].services.email.send({
+        to: email,
+        from: 'vencislav.developer@gmail.com',
+        subject: 'Email Confirmation',
+        text: `Please confirm your email by clicking on the following link: http://localhost:3000/confirmation?confirmation=${newUser.confirmationToken}`,
+        html: `<p>Please confirm your email by clicking on the following link:</p><a href="http://localhost:3000/confirmation?confirmation=${newUser.confirmationToken}">Confirm your email</a>`,
+      });
+    }
 
-    return ctx.send({ user: sanitizedUser });
+    const jwt = strapi.plugins['users-permissions'].services.jwt.issue({ id: newUser.id });
+
+    return ctx.send({ jwt, user: sanitizedUser });
   },
 };
